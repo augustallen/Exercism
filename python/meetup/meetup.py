607b2ab97e2b89f9ee6cb51be258277a38abd2d0
augustallen <august.allen@recursionpharma.com>
2020-08-17 11:25:22 -0600

    some progress

---
+++ python/meetup/meetup.py
@@ -0,0 +1,39 @@
+from datetime import date
+from calendar import monthrange
+
+def MeetupDayException(Exception):
+    pass
+
+days = {'Monday' : 0,
+'Tuesday' : 1,
+'Wednesday' : 2,
+'Thursday' : 3,
+'Friday' : 4,
+'Saturday' : 5,
+'Sunday' : 6
+}
+week_selection = {'1st' : 0, '2nd' : 1, '3rd' : 2, '4th' : 3, '5th' : 4}
+teenth_days = [13,14,15,16,17,18,19]
+
+def meetup(year, month, week, day_of_week):
+    # make list of all possible week days for the given day_of_week
+    n_days_in_month = [x for x in monthrange(year,month)]
+    days_matching_given_day_of_week = []
+    for d in range(1,n_days_in_month[1]+1):
+        if date(year, month, d).weekday() == days[day_of_week]:
+            days_matching_given_day_of_week.append(d)
+    # select the day of the week from the list
+    if week == "teenth":
+        day = list(set(teenth_days) & set(days_matching_given_day_of_week))[0]
+    else:
+        day = days_matching_given_day_of_week[week_selection[week]]
+    return date(year, month, day)
+
+
+
+received = meetup(2013, 4, "teenth", "Friday")
+
+print(f"Received: {received} of type {type(received)}")
+print(f"Expected: {date(2013,4,19)} of type {type(date(2013,4,19))}")
+
+print(type(date(2013,4,1)))
